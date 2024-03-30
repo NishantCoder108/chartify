e1991ae8c34306776a4347b7dbd1b55673f94b73
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-03-30 11:16:13 +0530

    custom color implemented

---
+++ src/lib/hexToRgba.ts
@@ -0,0 +1,11 @@
+export const hexToRgba = (hex: string, alpha: number) => {
+    const r = parseInt(hex.substring(1, 3), 16);
+    const g = parseInt(hex.substring(3, 5), 16);
+    const b = parseInt(hex.substring(5, 7), 16);
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+};
+
+// Usage
+const fillColorHex = "#ff0000"; // Your hex color
+const fillColorWithOpacity = hexToRgba(fillColorHex, 0.54);
+console.log({ fillColorWithOpacity });
