8d3a701c0e6c23d777a84c2988fb6170a04c6abd
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-03-30 12:08:35 +0530

    ♻️ code optimize in AppChart file

--- src/components/AppChart.tsx
+++ src/components/AppChart.tsx
@@ -1,27 +1,12 @@
-// import { ICountry } from "@/types/Country";
-import { debounce } from "@/lib/debounce";
 import { formatPopulation } from "@/lib/formatNumber";
 import { PointTooltipProps, ResponsiveLine } from "@nivo/line";
 import { useState } from "react";
-import * as d3 from "d3";
 import { customColors } from "@/lib/customColors";
 import { hexToRgba } from "@/lib/hexToRgba";
-
-interface Point {
-    x: string;
-    y: number;
-    yStacked: number;
-}
 
 interface IDataPoint {
     x: string;
     y: number;
-}
-
-interface IChartData {
-    id: string;
-    color?: string;
-    data: IDataPoint[];
 }
 
 interface IClickedPointData {
@@ -56,27 +41,9 @@ const AppChart = ({ data, changeChartData }: IProps) => {
     console.log({ data });
     const [selectedPoints, setSelectedPoints] = useState<IClickedPoint[]>([]);
 
-    // const handlePointClick = (point: IClickedPoint) => {
-    //     console.log({ point });
-    //     setSelectedPoint(point);
-    // };
-
-    // const handlePointClick = (point) => {
-    //     setSelectedPoints((prevSelectedPoints) => {
-    //         const pointIndex = prevSelectedPoints.findIndex(
-    //             (p) => p.x === point.data.x
-    //         );
-    //         if (pointIndex === -1) {
-    //             return [...prevSelectedPoints, point.data];
-    //         } else {
-    //             const updatedPoints = [...prevSelectedPoints];
-    //             updatedPoints.splice(pointIndex, 1);
-    //             return updatedPoints;
-    //         }
-    //     });
-    // };
-
     const handlePointClick = (point) => {
+        console.log("HandlePointclick", point);
+
         setSelectedPoints((prevSelectedPoints) => {
             const pointIndex = prevSelectedPoints.findIndex(
                 (p) => p.x === point.data.x
@@ -116,9 +83,6 @@ const AppChart = ({ data, changeChartData }: IProps) => {
     };
 
     console.log({ selectedPoints });
-    console.log(customColors.length);
-
-    // const debouncedHandlePointClick = debounce(handlePointClick, 250);
     return (
         <>
             <div className="h-56 cursor-pointer overflow-hidden">
@@ -161,70 +125,11 @@ const AppChart = ({ data, changeChartData }: IProps) => {
                     pointLabelYOffset={-12}
                     enableTouchCrosshair={true}
                     colors={{ scheme: "category10" }}
-                    // onClick={(point) =>
-                    //     debouncedHandlePointClick(point as IClickedPoint)
-                    // }
-
-                    // pointSymbol={(e) => {
-                    //     console.log({ e });
-                    //     let colorPunto = "";
-                    //     let colorBorde = "";
-
-                    //     switch (e.datum.x) {
-                    //         case "Eritrea":
-                    //             colorPunto = e.datum.valueI
-                    //                 ? "rgba(6,1,85,0.58)"
-                    //                 : "red";
-                    //             colorBorde = "rgb(6,1,85)";
-
-                    //             break;
-                    //         case "2":
-                    //             colorPunto = e.datum.valueI
-                    //                 ? "rgba(107,167,156,0.85)"
-                    //                 : "white";
-                    //             colorBorde = "rgba(42,146,77,1)";
-                    //             break;
-                    //     }
-
-                    //     return (
-                    //         <circle
-                    //             cx="0"
-                    //             cy="0"
-                    //             r="5"
-                    //             stroke={colorBorde}
-                    //             strokeWidth="2"
-                    //             fill={colorPunto}
-                    //         />
-                    //     );
-                    // }}
-
                     pointSymbol={(e) => {
                         const isSelected = selectedPoints.some(
                             (selectedPoint) => selectedPoint.x === e.datum.x
                         );
-                        // const colorPunto = isSelected ? "red" : "blue";
-                        // const colorBorde = isSelected
-                        //     ? "rgb(6,1,85)"
-                        //     : "rgba(6,1,85,0.58)";
 
-                        // return (
-                        //     <circle
-                        //         cx="0"
-                        //         cy="0"
-                        //         r="5"
-                        //         stroke={colorBorde}
-                        //         strokeWidth="2"
-                        //         fill={colorPunto}
-                        //     />
-                        // );
-
-                        // const customColors = [
-                        //     // Add your custom colors here
-                        //     "red",
-                        //     "blue",
-                        //     "green",
-                        //     "yellow",
-                        // ];
                         const color = isSelected
                             ? customColors[
                                   selectedPoints.findIndex(
@@ -237,9 +142,6 @@ const AppChart = ({ data, changeChartData }: IProps) => {
                                 cx="0"
                                 cy="0"
                                 r="3"
-                                // stroke={color}
-                                // strokeWidth="1"
-                                // fill={color}
                                 stroke={
                                     isSelected ? hexToRgba(color, 0.54) : "blue"
                                 }
@@ -248,63 +150,12 @@ const AppChart = ({ data, changeChartData }: IProps) => {
                             />
                         );
                     }}
-                    // pointColor={(point) => {
-                    //     const isSelected = selectedPoints.some(
-                    //         (selectedPoint) => selectedPoint.x === point.data.x
-                    //     );
-                    //     return isSelected ? "red" : "blue";
-                    // }}
                     onClick={handlePointClick}
-                    // pointColor={(point) => {
-                    //     const isSelected = selectedPoints.some(
-                    //         (selectedPoint) => {
-                    //             return point.data.some(
-                    //                 (p) => p.x === selectedPoint.x
-                    //             );
-                    //         }
-                    //     );
-                    //     return isSelected ? "red" : "blue";
-                    // }}
-                    // colors={["#00a9ff", "#ff0000"]}
                     onMouseEnter={(_datum, event) => {
                         (event.currentTarget as HTMLElement).style.cursor =
                             "pointer";
                     }}
                     useMesh={true}
-                    // layers={[
-                    //     "grid",
-                    //     "markers",
-                    //     "axes",
-                    //     "areas",
-                    //     "crosshair",
-                    //     "lines",
-                    //     "points",
-                    //     ({ points }) => (
-                    //         <g>
-                    //             {points.map(
-                    //                 (point: {
-                    //                     id: string;
-                    //                     x: number;
-                    //                     y: number;
-                    //                 }) => (
-                    //                     <circle
-                    //                         key={point.id}
-                    //                         cx={point.x}
-                    //                         cy={point.y}
-                    //                         r={10}
-                    //                         fill="transparent"
-                    //                         className="cursor-pointer"
-                    //                         onClick={() =>
-                    //                             debouncedHandlePointClick(
-                    //                                 point as IClickedPoint
-                    //                             )
-                    //                         }
-                    //                     />
-                    //                 )
-                    //             )}
-                    //         </g>
-                    //     ),
-                    // ]}
                     theme={{
                         crosshair: {
                             line: {
@@ -323,32 +174,34 @@ const AppChart = ({ data, changeChartData }: IProps) => {
                     }}
                     crosshairType="x"
                     tooltip={CustomTooltip}
-                    // legends={[
-                    //     {
-                    //         anchor: "bottom-right",
-                    //         direction: "column",
-                    //         justify: false,
-                    //         translateX: 100,
-                    //         translateY: 0,
-                    //         itemsSpacing: 0,
-                    //         itemDirection: "left-to-right",
-                    //         itemWidth: 80,
-                    //         itemHeight: 20,
-                    //         itemOpacity: 0.75,
-                    //         symbolSize: 12,
-                    //         symbolShape: "circle",
-                    //         symbolBorderColor: "rgba(0, 0, 0, .5)",
-                    //         effects: [
-                    //             {
-                    //                 on: "hover",
-                    //                 style: {
-                    //                     itemBackground: "rgba(0, 0, 0, .03)",
-                    //                     itemOpacity: 1,
-                    //                 },
-                    //             },
-                    //         ],
-                    //     },
-                    // ]}
+                    legends={
+                        [
+                            // {
+                            //     anchor: "bottom-right",
+                            //     direction: "column",
+                            //     justify: false,
+                            //     translateX: 100,
+                            //     translateY: 0,
+                            //     itemsSpacing: 0,
+                            //     itemDirection: "left-to-right",
+                            //     itemWidth: 80,
+                            //     itemHeight: 20,
+                            //     itemOpacity: 0.75,
+                            //     symbolSize: 12,
+                            //     symbolShape: "circle",
+                            //     symbolBorderColor: "rgba(0, 0, 0, .5)",
+                            //     effects: [
+                            //         {
+                            //             on: "hover",
+                            //             style: {
+                            //                 itemBackground: "rgba(0, 0, 0, .03)",
+                            //                 itemOpacity: 1,
+                            //             },
+                            //         },
+                            //     ],
+                            // },
+                        ]
+                    }
                 />
             </div>
 
